eb7eca7129d5e8d487dc47d82929a43dfa069b97
yian <yian31415@cs.nctu.edu.tw>
2022-03-09 13:32:01 +0800

    producer/oauth.go gofumpt

--- producer/oauth.go
+++ producer/oauth.go
@@ -1,27 +1,26 @@
 package producer
 
 import (
+	"io/ioutil"
 	"net/http"
 	"strings"
-	"io/ioutil"
-	//"fmt"
-
-	"github.com/free5gc/nrf/logger"
-	"github.com/free5gc/http_wrapper"
 
 	"github.com/dgrijalva/jwt-go"
+
+	"github.com/free5gc/http_wrapper"
+	"github.com/free5gc/nrf/logger"
 )
 
 type AccessTokenClaims struct {
-  Iss             string                  // TODO: NF instance id of the NRF
-  Sub             string                  // nfInstanceId of service consumer
-  Aud             string                  // nfInstanceId of service producer
-  Scope           string                  // TODO: the name of the NF services for which the
-  Exp             int32                   // access_token is authorized for use
-  *jwt.StandardClaims
+	Iss   string // TODO: NF instance id of the NRF
+	Sub   string // nfInstanceId of service consumer
+	Aud   string // nfInstanceId of service producer
+	Scope string // TODO: the name of the NF services for which the
+	Exp   int32  // access_token is authorized for use
+	*jwt.StandardClaims
 }
 
-func OAuthVerify (request *http_wrapper.Request, serviceName string, pubKeyPath string) *http_wrapper.Response {
+func OAuthVerify(request *http_wrapper.Request, serviceName string, pubKeyPath string) *http_wrapper.Response {
 	verifyBytes, err := ioutil.ReadFile(pubKeyPath)
 	if err != nil {
 		logger.ManagementLog.Infoln("VerifyBytes failed")
@@ -37,18 +36,17 @@ func OAuthVerify (request *http_wrapper.Request, serviceName string, pubKeyPath 
 	token, err := jwt.ParseWithClaims(tokenString, &AccessTokenClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return verifyKey, nil
 	})
-
 	if err != nil {
 		logger.ManagementLog.Infoln(err.Error())
 		return http_wrapper.NewResponse(http.StatusUnauthorized, nil, err)
 	}
 
-	if VerifyScope(token.Claims.(*AccessTokenClaims).Scope, serviceName) == false {
+	if !VerifyScope(token.Claims.(*AccessTokenClaims).Scope, serviceName) {
 		return http_wrapper.NewResponse(http.StatusForbidden, nil, nil)
 	}
 	return nil
 }
 
-func VerifyScope (scope string, serviceName string) bool {
+func VerifyScope(scope string, serviceName string) bool {
 	return strings.Contains(scope, serviceName)
 }
